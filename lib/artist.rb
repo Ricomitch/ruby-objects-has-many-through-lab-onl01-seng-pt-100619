1b3aac348aaaf42c75c117ef7251b48ddf6f1fa2
Rico Mitchell <ricomitch@icloud.com>
2019-11-15 02:36:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,26 @@
-class Artist 
+class Artist
   attr_accessor :name
   @@all = []
   
   def initialize(name)
     @name = name
-    @all << self
-  end
-
-  def self.all
-    @@all
+    @@all << self
   end
   
-  def new_song(name, genre)
-    Song.new(name, self, genre)
+  def self.all
+    @@all
   end
   
   def songs
     Song.all.select {|song| song.artist == self}
   end
   
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+  
   def genres
     songs.collect(&:genre)
   end
-  
   
 end
